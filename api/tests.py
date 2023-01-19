2b40a12997a36632cac8d90b628b77e6b2691d46
Jaris Melo <jamcyber@hotmail.com>
2023-01-19 00:57:43 -0500

    Test creation

--- api/tests.py
+++ api/tests.py
@@ -1,3 +1,74 @@
-from django.test import TestCase
+from rest_framework import status
+from rest_framework.test import APITestCase
+from rest_framework.test import APIClient
+from .models import User
+from django.contrib.auth import authenticate
 
-# Create your tests here.
+class BankTests(APITestCase):
+    def test_post_unauthenticated_bank(self):
+        data = {'bank_name': 'Suramericano'}
+        response = self.client.post('/api/bank', data, format='json')
+        self.assertEqual(response.status_code, status.HTTP_401_UNAUTHORIZED)
+    
+    def test_create_bank(self):
+        user = User.objects.create_user('test@test.com', 'testsecret')
+        self.client = APIClient()
+        self.client.force_authenticate(user=user) 
+             
+        data = {'bank_name': 'Suramericano'}
+        response = self.client.post('/api/bank', data, format='json')
+        self.assertEqual(response.status_code, status.HTTP_201_CREATED)
+     
+    def test_get_bank(self):
+        user = User.objects.create_user('test@test.com', 'testsecret')
+        self.client = APIClient()
+        self.client.force_authenticate(user=user) 
+             
+        data = {'bank_name': 'Suramericano'}
+        self.client.post('/api/bank', data, format='json')
+        
+        data1 = {'bank_name': 'Hispanoamericano'}
+        self.client.post('/api/bank', data1, format='json')
+        
+        response = self.client.get('/api/bank', format='json')
+        self.assertEqual(response.status_code, status.HTTP_200_OK)
+
+      
+    def test_get_a_bank(self):
+        user = User.objects.create_user('test@test.com', 'testsecret')
+        self.client = APIClient()
+        self.client.force_authenticate(user=user) 
+             
+        data = {'bank_name': 'Suramericano'}
+        response = self.client.post('/api/bank', data, format='json')
+        response_json = response.json()
+
+        response = self.client.get(f'/api/bank-CRUD/{response_json["id"]}', format='json')
+        self.assertEqual(response.status_code, status.HTTP_200_OK)
+
+    
+    def test_update_bank(self):
+        user = User.objects.create_user('test@test.com', 'testsecret')
+        self.client = APIClient()
+        self.client.force_authenticate(user=user) 
+        
+        data = {'bank_name': 'Suramericano'}
+        response = self.client.post('/api/bank', data, format='json')
+        response_json = response.json()
+        
+        data_update = {'bank_name': 'Centroamericano'}
+        response = self.client.put(f'/api/bank-CRUD/{response_json["id"]}', data_update, format='json')
+        self.assertEqual(response.status_code, status.HTTP_200_OK)
+       
+
+    def test_delete_bank(self):
+        user = User.objects.create_user('test@test.com', 'testsecret')
+        self.client = APIClient()
+        self.client.force_authenticate(user=user) 
+             
+        data = {'bank_name': 'Suramericano'}
+        response = self.client.post('/api/bank', data, format='json')
+        response_json = response.json()
+        
+        response = self.client.delete(f'/api/bank-CRUD/{response_json["id"]}', format='json')
+        self.assertEqual(response.status_code, status.HTTP_204_NO_CONTENT)
