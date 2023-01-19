324814715919d7004ca8f16a07402401a2f62436
Jaris Melo <jamcyber@hotmail.com>
2023-01-19 01:47:04 -0500

    Test creation

--- api/tests.py
+++ api/tests.py
@@ -2,7 +2,6 @@ from rest_framework import status
 from rest_framework.test import APITestCase
 from rest_framework.test import APIClient
 from .models import User
-from django.contrib.auth import authenticate
 
 class BankTests(APITestCase):
     def test_post_unauthenticated_bank(self):
@@ -71,4 +70,107 @@ class BankTests(APITestCase):
         response_json = response.json()
         
         response = self.client.delete(f'/api/bank-CRUD/{response_json["id"]}', format='json')
+        self.assertEqual(response.status_code, status.HTTP_204_NO_CONTENT)
+        
+class ProviderTests(APITestCase):
+    def test_post_unauthenticated_provider(self):
+        data = {
+                "provider_name": "Laura Buenaventura S.A.S.",
+                "nit_provider": "901866782-2",
+                "contact_name": "Rodolfo Castro",
+                "cell_phone_contact": "3006503944"
+        }
+        response = self.client.post('/api/provider', data, format='json')
+        self.assertEqual(response.status_code, status.HTTP_401_UNAUTHORIZED)
+    
+        
+    def test_create_provider(self):
+        user = User.objects.create_user('test@test.com', 'testsecret')
+        self.client = APIClient()
+        self.client.force_authenticate(user=user) 
+
+        data = {
+                "provider_name": "Laura Buenaventura S.A.S.",
+                "nit_provider": "901866782-2",
+                "contact_name": "Rodolfo Castro",
+                "cell_phone_contact": "3006503944"
+        }
+        response = self.client.post('/api/provider', data, format='json')
+        self.assertEqual(response.status_code, status.HTTP_201_CREATED)
+
+    
+    def test_get_provider(self):
+        user = User.objects.create_user('test@test.com', 'testsecret')
+        self.client = APIClient()
+        self.client.force_authenticate(user=user) 
+             
+        data = {
+                "provider_name": "Laura Buenaventura S.A.S.",
+                "nit_provider": "901866782-2",
+                "contact_name": "Rodolfo Castro",
+                "cell_phone_contact": "3006503944"
+        }
+        self.client.post('/api/provider', data, format='json')
+        
+        response = self.client.get('/api/provider', format='json')
+        self.assertEqual(response.status_code, status.HTTP_200_OK)
+
+     
+    def test_get_a_provider(self):
+        user = User.objects.create_user('test@test.com', 'testsecret')
+        self.client = APIClient()
+        self.client.force_authenticate(user=user) 
+        
+        data = {
+                "provider_name": "Laura Buenaventura S.A.S.",
+                "nit_provider": "901866782-2",
+                "contact_name": "Rodolfo Castro",
+                "cell_phone_contact": "3006503944"
+        }
+        response = self.client.post('/api/provider', data, format='json')
+        response_json = response.json()
+
+        response = self.client.get(f'/api/provider-CRUD/{response_json["id"]}', format='json')
+        self.assertEqual(response.status_code, status.HTTP_200_OK)
+
+    
+    def test_update_provider(self):
+        user = User.objects.create_user('test@test.com', 'testsecret')
+        self.client = APIClient()
+        self.client.force_authenticate(user=user) 
+        
+        data = {
+                "provider_name": "Laura Buenaventura S.A.S.",
+                "nit_provider": "901866782-2",
+                "contact_name": "Rodolfo Castro",
+                "cell_phone_contact": "3006503944"
+        }
+        response = self.client.post('/api/provider', data, format='json')
+        response_json = response.json()
+        
+        data_update = {
+                "provider_name": "Marcela Steel S.A.",
+                "nit_provider": "902566641-3",
+                "contact_name": "Brayan Mendoza",
+                "cell_phone_contact": "3004301911"
+        }
+        response = self.client.put(f'/api/provider-CRUD/{response_json["id"]}', data_update, format='json')
+        self.assertEqual(response.status_code, status.HTTP_200_OK)
+
+
+    def test_delete_provider(self):
+        user = User.objects.create_user('test@test.com', 'testsecret')
+        self.client = APIClient()
+        self.client.force_authenticate(user=user) 
+        
+        data = {
+                "provider_name": "Laura Buenaventura S.A.S.",
+                "nit_provider": "901866782-2",
+                "contact_name": "Rodolfo Castro",
+                "cell_phone_contact": "3006503944"
+        }
+        response = self.client.post('/api/provider', data, format='json')
+        response_json = response.json()
+        
+        response = self.client.delete(f'/api/provider-CRUD/{response_json["id"]}', format='json')
         self.assertEqual(response.status_code, status.HTTP_204_NO_CONTENT)
